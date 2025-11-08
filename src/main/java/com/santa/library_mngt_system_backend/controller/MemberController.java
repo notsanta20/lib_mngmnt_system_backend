d2a961d6feca4def7a6eb1a2209a876c2df3444e
santhosh <santhosh.003z@gmail.com>
2025-11-08 18:04:42 +0530

    feat: added member table and rest CRUD operations

---
+++ src/main/java/com/santa/library_mngt_system_backend/controller/MemberController.java
@@ -0,0 +1,63 @@
+package com.santa.library_mngt_system_backend.controller;
+
+import com.santa.library_mngt_system_backend.model.Member;
+import com.santa.library_mngt_system_backend.model.Transaction;
+import com.santa.library_mngt_system_backend.repo.MemberRepo;
+import com.santa.library_mngt_system_backend.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MemberController {
+
+    @Autowired
+    private MemberService service;
+
+    @GetMapping("/members")
+    public ResponseEntity<List<Member>> getAllMembers(){
+        List<Member> membersList = service.getAllMembers();
+
+        return new ResponseEntity<>(membersList, HttpStatus.OK);
+    }
+
+    @PostMapping("/members")
+    public ResponseEntity<String> addMember(@RequestBody Member member){
+        service.addMember(member);
+
+        return new ResponseEntity<>("Member registered successfully", HttpStatus.OK);
+    }
+
+    @GetMapping("/members/{id}")
+    public ResponseEntity<Member> getMemberById(@PathVariable long id){
+        Member member = service.getMemberById(id);
+
+        return new ResponseEntity<>(member, HttpStatus.OK);
+    }
+
+    @PutMapping("/members/{id}")
+    public ResponseEntity<String> updateMemberById(@PathVariable long id, @RequestBody Member member){
+        service.updateMemberById(id, member);
+
+        return new ResponseEntity<>("Member updated successfully", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/members/{id}")
+    public ResponseEntity<String> deleteMemberById(@PathVariable long id){
+        service.deleteMemberById(id);
+
+        return new ResponseEntity<>("Member deleted successfully", HttpStatus.OK);
+    }
+
+    @GetMapping("/members/{id}/history")
+    public ResponseEntity<List<Transaction>> getMemberHistoryById(@PathVariable long id){
+        List<Transaction> memberHistory = service.getMemberHistoryById(id);
+
+        return new ResponseEntity<>(memberHistory, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/santa/library_mngt_system_backend/service/MemberService.java
@@ -0,0 +1,53 @@
+package com.santa.library_mngt_system_backend.service;
+
+import com.santa.library_mngt_system_backend.model.Member;
+import com.santa.library_mngt_system_backend.model.Transaction;
+import com.santa.library_mngt_system_backend.repo.MemberRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MemberService {
+
+    @Autowired
+    private MemberRepo repo;
+
+    public List<Member> getAllMembers() {
+        return repo.findAll();
+    }
+
+    public void addMember(Member member) {
+        repo.save(member);
+    }
+
+    public Member getMemberById(long id) {
+        return repo.findById(id).orElse(new Member());
+    }
+
+    public void updateMemberById(long id, Member member) {
+        Member updateMember = getMemberById(id);
+        System.out.println(updateMember);
+        updateMember.setName(member.getName());
+        updateMember.setEmail(member.getEmail());
+        updateMember.setPhone(member.getPhone());
+        updateMember.setMembershipType(member.getMembershipType());
+        updateMember.setMembershipDate(member.getMembershipDate());
+        updateMember.setExpiryDate(member.getExpiryDate());
+        updateMember.setTotalFines(member.getTotalFines());
+        updateMember.setActive(member.isActive());
+
+        repo.save(updateMember);
+    }
+
+    public void deleteMemberById(long id) {
+        repo.deleteById(id);
+    }
+
+    public List<Transaction> getMemberHistoryById(long id) {
+        Member member = repo.findById(id).orElse(new Member());
+
+        return member.getTransactions();
+    }
+}
