17e192ad0740abf34021722ab8e2f5cc77bb33ae
santhosh <santhosh.003z@gmail.com>
2025-11-08 12:49:42 +0530

    feat: added basic structure

---
+++ src/main/java/com/santa/library_mngt_system_backend/LibraryMngtSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.santa.library_mngt_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMngtSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryMngtSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santa/library_mngt_system_backend/LibraryMngtSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santa.library_mngt_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMngtSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
