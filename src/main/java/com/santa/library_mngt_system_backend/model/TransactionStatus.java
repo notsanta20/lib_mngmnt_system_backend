e6d68a5459c8b3a1d5c3d4fe49d819eb03f29d1e
santhosh <santhosh.003z@gmail.com>
2025-11-11 21:26:38 +0530

    migrated to microservices

--- src/main/java/com/santa/library_mngt_system_backend/model/TransactionStatus.java
+++
@@ -1,5 +0,0 @@
-package com.santa.library_mngt_system_backend.model;
-
-public enum TransactionStatus {
-    ISSUED,RETURNED,OVERDUE
-}
--- src/main/java/com/santa/library_mngt_system_backend/repo/MemberRepo.java
+++
@@ -1,9 +0,0 @@
-package com.santa.library_mngt_system_backend.repo;
-
-import com.santa.library_mngt_system_backend.model.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepo extends JpaRepository<Member, Long> {
-}
