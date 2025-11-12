bdd7f2d6f177eab62cfab80bc885f8a7f5d78a05
santhosh <santhosh.003z@gmail.com>
2025-11-12 18:31:45 +0530

    update: updated parameter from id to isbn

--- book-service/src/main/java/com/santa/book_service/exception/BookNotFoundException.java
+++ book-service/src/main/java/com/santa/book_service/exception/BookNotFoundException.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class BookNotFoundException extends RuntimeException{
-    public BookNotFoundException(long id){
-        super("Book not found with ID: " + id);
+    public BookNotFoundException(String isbn){
+        super("Book not found with ID: " + isbn);
     }
 }
