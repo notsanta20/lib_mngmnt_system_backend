ca36c025b819b9a18bbc4565199c813ee7af48e1
santhosh <santhosh.003z@gmail.com>
2025-11-12 13:17:40 +0530

    update: udpated the auth controller

--- auth-service/src/main/java/com/santa/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/com/santa/auth_service/controller/AuthController.java
@@ -1,9 +1,10 @@
 package com.santa.auth_service.controller;
 
 import com.santa.auth_service.dto.UserDTO;
-import com.santa.auth_service.service.UserService;
+import com.santa.auth_service.dto.UserLoginDTO;
+import com.santa.auth_service.dto.UserRegisterDTO;
+import com.santa.auth_service.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,20 +14,17 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     @Autowired
-    private UserService service;
+    private AuthService service;
 
-    @GetMapping("/users")
-    public Page<UserDTO> getUsers(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size
-    ){
-        return service.getAllUsers(page,size);
+    @PostMapping("/register")
+    public ResponseEntity<UserDTO> getUsers(@RequestBody UserRegisterDTO user){
+        UserDTO newUser = service.registerUser(user);
+        return new ResponseEntity<>(newUser, HttpStatus.OK);
     }
 
-    @GetMapping("/users/{username}")
-    public ResponseEntity<UserDTO> getUserByUsername(@PathVariable String username){
-        UserDTO user = service.getUserByUsername(username);
+    @PostMapping("/login")
+    public ResponseEntity<String> getUserByUsername(@RequestBody UserLoginDTO user){
 
-        return new ResponseEntity<>(user, HttpStatus.OK);
+        return new ResponseEntity<>("logged in", HttpStatus.OK);
     }
 }
